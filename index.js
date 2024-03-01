6e8388998ce065b7e71e2d7792a94fa6018dc571
ClassicDew <107551134+Krictthep@users.noreply.github.com>
2024-03-01 17:38:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 4000;
 
 app.use(express.urlencoded({extends: true}))
 app.use(express.json())
