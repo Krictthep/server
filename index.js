4e32c91ca2545a6c5768192244a6235f78e82edc
krictthep <krictthep@gmail.com>
2024-03-02 23:40:51 +0700

    add cors

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port = 8000
 
 app.use(express.urlencoded({extends: true}))
 app.use(express.json())
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // const { default: mongoose } = require('mongoose');
